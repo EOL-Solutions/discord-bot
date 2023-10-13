50e3683c4e9fbd28a8336e13d7fc99d94c6cbbaa
ddaniel27 <danieldorado27@gmail.com>
2023-10-13 12:10:47 -0500

    [UPDATE] Migrations 3

--- commands/timearBobitos/exec.js
+++ commands/timearBobitos/exec.js
@@ -1,53 +1,38 @@
-module.exports = async (interaction, _repository, client) => {
+module.exports = async (interaction, repository, client) => {
   try {
+    const { configurationsRepository } = repository
+    const { value } = await configurationsRepository.findConfigurationByField('timeout-time')
+    let total = 1000 * 60
+    const time = Number(interaction.options.getString('time'))
+    if (isNaN(time) || time <= 0) {
+      total *= value
+    } else {
+      total *= time
+    }
 
-    const channels = client
-      .channels
-      .cache
-      .filter(channel => channel.isVoiceBased())
+    const guilds = await client.guilds.fetch()
+    const guildsFetched = await Promise.allSettled(guilds.map(guild => guild.fetch()))
+    const channelsGeneral = await Promise.allSettled(guildsFetched.map(guild => guild.value.channels.fetch()))
+    const channels = channelsGeneral.map(channel => 
+      Array.from(channel.value.filter(channel => 
+        channel.isVoiceBased())
+        .values()))
+        .flat()
 
-    const channelsFromFetch = await Promise.allSettled(channels.map(channel => channel.fetch()))
-    console.log(channelsFromFetch)
+    const channelsFetched = await Promise.allSettled(channels.map(channel => channel.fetch()))
+    const bobitos = channelsFetched
+      .filter(channel => channel.status === 'fulfilled')
+      .map(channel => channel.value)
+      .map(channel => channel.members.map(member => member).flat())
+      .filter(members => members.length > 0)
+      .flat()
+    const bobito = bobitos.sort(() => 0.5 - Math.random()).slice(0, 1)[0]
+    await bobito.disableCommunicationUntil(new Date(Date.now() + total))
+    interaction.deferReply()
+    interaction.deleteReply()
+    interaction.channel.send({ content: `Estan timeando bobitos, cierto ${bobito.displayName}` })
   } catch (error) {
     console.error(error)
     return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
   }
-  /*
-  const time = Number(interaction.options.getString('time')) === 0 ? 1 : Number(interaction.options.getString('time'))
-  if (isNaN(time)) {
-    return interaction.reply({ content: 'El tiempo debe ser un número, bobito', ephemeral: true })
-  }
-  if (isNaN(Number(config["timeout-time"]))) {
-    return interaction.reply({ content: 'El tiempo de timeout debe ser un número, bobito', ephemeral: true })
-  }
-
-  const total = time > 0 ? Math.floor(time * 60 * 1000) : Math.floor(Number(config["timeout-time"]) * 60 * 1000)
-
-    .then(channels => {
-      const bobitos = []
-      bobitos.push(
-        ...channels
-          .filter(channel => channel.status === 'fulfilled')
-          .map(channel => channel.value)
-          .map(channel => channel.members.map(member => member))
-          .flat())
-      return bobitos
-    })
-    .then(bobitos => {
-      const bobito = bobitos.sort(() => 0.5 - Math.random()).slice(0, 1)[0]
-      bobito
-        .disableCommunicationUntil(new Date(Date.now() + total))
-        .then(() => {
-          interaction.deferReply()
-          interaction.deleteReply()
-          interaction.channel.send({ content: `Estan timeando bobitos, cierto ${bobito.displayName}` })
-        })
-        .catch(() => {
-          interaction.deferReply()
-          interaction.deleteReply()
-          interaction.channel.send({ content: `No se pudo timear este bobito: ${bobito.displayName}` })
-        })
-    })
-    .catch(console.error)
-  */
 }
