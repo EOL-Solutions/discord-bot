1397fe9f1666fd4a57539d9b5a8d99115449cd7d
ddaniel27 <danieldorado27@gmail.com>
2023-11-03 18:59:21 -0500

    [UPDATE] Added event

--- client-events/voiceStateUpdate.js
+++ client-events/voiceStateUpdate.js
@@ -6,25 +6,36 @@ let started = false
 module.exports = (_repositories) => {
   const event = async (oldState, newState) => {
     const { channel: newChannel, guild: newGuild } = newState
-    const { channel: oldChannel } = oldState
-    if (oldChannel === null || newChannel === null) return
-    if (oldChannel.id === newChannel.id) return
+    const { channel: oldChannel, guild: oldGuild } = oldState
+    if (oldChannel?.id === newChannel?.id) return
 
-    if (oldChannel.id !== TARGET_CHANNEL_ID && newChannel.id === TARGET_CHANNEL_ID) { // user joined the channel
+    if (newChannel !== null && newChannel.id === TARGET_CHANNEL_ID) { // user joined the channel
       if (newChannel.members.size >= MINIMUM_MEMBER_COUNT && !started) {
         started = true
-        const { channels } = newGuild
+        const { channels, scheduledEvents } = newGuild
         const textChannel = await channels.cache.get(TEXT_CHANNEL_ID)
         await textChannel.send(`Convencion de cacorros: ${newChannel.members.map(member => member.displayName).join(', ')}`)
-        /*
-        const { scheduledEvents } = newGuild
-        const events = await scheduledEvents.cache
-        console.log(events)
-        */
+        const event = await scheduledEvents.create({
+          name: 'Convencion de cacorros',
+          scheduledStartTime: new Date(Date.now()),
+          privacyLevel: 2,
+          entityType: 2,
+          description: `${newChannel.members.map(member => member.displayName).join(', ')} estan en convencion`,
+          channel: newChannel,
+          reason: 'Mucho cacorro en el grupo'
+        })
+        await event.setStatus(2, 'Iniciado por bot')
       }
-    } else if (oldChannel.id === TARGET_CHANNEL_ID && newChannel.id !== TARGET_CHANNEL_ID) { // user left the channel
+    } else if (oldChannel !== null && oldChannel.id === TARGET_CHANNEL_ID) { // user left the channel
       if (oldChannel.members.size < MINIMUM_MEMBER_COUNT) {
         started = false
+        const { channels, scheduledEvents } = oldGuild
+        const textChannel = await channels.cache.get(TEXT_CHANNEL_ID)
+        await textChannel.send('Convencion de cacorros terminada')
+        const events = await scheduledEvents.fetch()
+        events.forEach(async event => {
+          if (event.creator.bot) { await event.delete() }
+        })
       }
     }
   }
